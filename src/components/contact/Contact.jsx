9d4415d6de7eaf5ebbaebe2e4779468999370747
RafiBinWores <rafibinwores2203@gmail.com>
2024-12-30 18:20:22 +0600

    Contact updated

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -25,7 +25,7 @@ function Contact() {
         import.meta.env.VITE_EMAILJS_SERVICE_ID,
         import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
         data,
-        import.meta.env.VITE_EMAILJS_PUBLIC_KEY
+        import.meta.env.VITE_EMAILJS_USER_ID
       )
       .then(
         () => {
