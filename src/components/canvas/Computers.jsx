34b1c1ef0e8e99bb1ed6ac0e80feed8867627979
Rafi Bin Wores <rafibinwores2203@gmail.com>
2025-11-01 12:15:42 +0600

    [UPDATE] Hero section updated

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -20,8 +20,8 @@ const Computers = ({isMobile}) => {
       <pointLight intensity={2} />
       <primitive
         object={computer.scene}
-        scale={isMobile ? 0.7 : 0.75}
-        position={isMobile? [0, -3, -2.1]:[0, -2.8, -1.5]}
+        scale={isMobile ? 0.6 : 0.6}
+        position={isMobile? [0, -2.7, -2.1]:[0, -2.8, -1.5]}
       />
     </mesh>
   );
